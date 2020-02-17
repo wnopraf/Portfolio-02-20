e8cf8ad9bb23276f6f0a60dd028ccde652745096
wnpraf <rapribu@gmail.com>
2020-02-17 17:16:06 +0100

    Add configuration files

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports =  {
+    parser:  '@typescript-eslint/parser',  
+    extends:  [
+      'plugin:react/recommended',  
+      'plugin:@typescript-eslint/recommended',  
+      'prettier/@typescript-eslint',
+      'plugin:prettier/recommended'
+    ],
+    parserOptions:  {
+    ecmaVersion:  2018,  
+    sourceType:  'module',  
+    ecmaFeatures:  {
+      jsx:  true,  
+    }
+    },
+    rules:  {
+      "react/react-in-jsx-scope": "off",
+      "react/display-name": "off"
+    },
+    settings:  {
+      react:  {
+        version:  'detect',  
+      }
+    }
+  }
