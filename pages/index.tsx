65f45ec581d0b726028521b53a35483d6f222d4b
wnpraf <rapribu@gmail.com>
2020-02-18 12:02:58 +0100

    Add letters component to header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
 import ResponisveMenu from '../components/ResponisveMenu'
 import ResponsiveMenuHr from '../components/ResponsiveMenuHr'
+import Letters from '../components/Letters'
 
 export default () => {
   return (
     <>
       <header className="">
-        <div className="container mx-auto"></div>
+        <div className="container mx-auto">
+          <Letters name="Rafael Prieto" />
+        </div>
         <ResponsiveMenuHr />
       </header>
     </>
