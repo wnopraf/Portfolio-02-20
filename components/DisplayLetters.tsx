d58e6fd2cadc2de18026a01d6051a5b114ee5503
wnpraf <rapribu@gmail.com>
2020-02-18 17:10:34 +0100

    Add DisplayLeeter component

---
+++ components/DisplayLetters.tsx
@@ -0,0 +1,18 @@
+import { useRef } from 'react'
+import { useChain } from 'react-spring'
+import LettersName from './LettersName'
+import LettersProfession from './LettersProfession'
+
+export default () => {
+  const nameRef = useRef()
+  const professionRef = useRef()
+
+  useChain([nameRef, professionRef])
+
+  return (
+    <div>
+      <LettersName name="Rafael Prieto" nameRef={nameRef} />
+      <LettersProfession profession="Web Developer" professionRef={professionRef} />
+    </div>
+  )
+}
