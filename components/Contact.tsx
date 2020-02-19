99fa40e160abae3b4533cf6bde5ddb624b3feb18
wnpraf <rapribu@gmail.com>
2020-02-19 22:10:23 +0100

    Add Contact component

---
+++ components/Contact.tsx
@@ -0,0 +1,8 @@
+import { MdContactMail } from 'react-icons/md'
+export default () => {
+  return (
+    <div className="contact flex justify-center items-center text-white text-2xl font-extrabold">
+      <MdContactMail className=" w-24 h-24" /> <p className="ml-3">rapribu@gmail.com</p>
+    </div>
+  )
+}
