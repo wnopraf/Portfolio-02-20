bb9fdbe5e7e70775c095b5f3f5c7495c75affc20
wnpraf <rapribu@gmail.com>
2020-02-20 17:24:37 +0100

    Add some responsive touches to Contact component

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,8 +1,8 @@
 import { MdContactMail } from 'react-icons/md'
 export default () => {
   return (
-    <div className="contact flex justify-center items-center text-white text-2xl font-extrabold">
-      <MdContactMail className=" w-24 h-24" /> <p className="ml-3">rapribu@gmail.com</p>
+    <div className="contact flex flex-col sm:flex-row justify-center items-center text-white text-2xl font-extrabold">
+      <MdContactMail className=" w-24 h-24" /> <p className="mt-6 sm:mt-0 sm:ml-6 ">rapribu@gmail.com</p>
     </div>
   )
 }
